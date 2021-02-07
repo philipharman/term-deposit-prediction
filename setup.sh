f4e528182bed9586e65ab23dffd5e21277b4ddc2
philipharman <43306247+philipharman@users.noreply.github.com>
2021-02-07 18:09:09 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,9 @@
-# mkdir -p ~/.streamlit/
+mkdir -p ~/.streamlit/
 
-# heroku ps:scale web=1
-
-# echo "\
-# [server]\n\
-# port = $PORT\n\
-# enableCORS = false\n\
-# headless = true\n\
-# \n\
-# " > ~/.streamlit/config.toml
-web: gunicorn bot:app
+echo "\
+[server]\n\
+port = $PORT\n\
+enableCORS = false\n\
+headless = true\n\
+\n\
+" > ~/.streamlit/config.toml
